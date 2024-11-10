9c46a702c02e144b84ab3a685cae87f1d268e9a8
Achala Rao <140195893+Achala1920@users.noreply.github.com>
2024-11-10 15:18:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
 
     try {
       const response = await axios.post(
-        'https://dgsc1pirbg.execute-api.us-east-1.amazonaws.com/dev/generate-blog',
+        'https://dzqi68u588.execute-api.us-east-1.amazonaws.com/dev/generate-blog',
         { prompt }
       );
       setBlogData(response.data);
